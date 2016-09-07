24ba85b227653842a032aae782a065fc03ca3d55
iisergeev <iisergeev@alfabank.ru>
2016-09-07 16:32:14 +0300

    refactoring

--- analyser/src/main/java/jocker/analyser/CountExceptionPerSecondBot.java
+++ analyser/src/main/java/jocker/analyser/CountExceptionPerSecondBot.java
@@ -23,24 +23,23 @@ public class CountExceptionPerSecondBot {
         settings.put(StreamsConfig.ZOOKEEPER_CONNECT_CONFIG, "192.168.99.100:2182");
         settings.put(StreamsConfig.TIMESTAMP_EXTRACTOR_CLASS_CONFIG,  MyEventTimeExtractor.class.getName());
 
-        StreamsConfig config = new StreamsConfig(settings);
-        KStreamBuilder kStreamBuilder = new KStreamBuilder();
-
-        KStream<String, String> kStream =  kStreamBuilder.stream(
-                new Serdes.StringSerde(), new Serdes.StringSerde(), "exceptions");
-
-        KTable<Windowed<String>, Long> exceptionCounts =
-                kStream.countByKey(
-                        TimeWindows.of("window", 1000L)
-                                .advanceBy(100L),
-                        Serdes.String());
-
         SlackSession session = SlackSessionFactory.createWebSocketSlackSession("xoxb-77064816199-66jVEUcogKW7ROd1HPQlytpH");
         session.connect();
         SlackChannel channel = session.findChannelByName("general");
 
-        exceptionCounts.toStream((k, v) -> k.key()).process(
-                () -> new AbstractProcessor<String, Long>() {
+        StreamsConfig config = new StreamsConfig(settings);
+        KStreamBuilder kStreamBuilder = new KStreamBuilder();
+
+        KStream<String, String> kStream = kStreamBuilder.stream(
+                new Serdes.StringSerde(), new Serdes.StringSerde(), "info");
+
+        kStream.filter((k, v) -> v.contains("exception")).
+                countByKey(
+                        TimeWindows.of("window", 1000L)
+                                .advanceBy(500L),
+                        Serdes.String())
+                .toStream((k, v) -> k.key())
+                .process(() -> new AbstractProcessor<String, Long>() {
                     @Override
                     public void process(String key, Long value) {
                         String info = "exception " + (key) + " " + (value);
